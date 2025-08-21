35c590b1a3954cf3f6ce4464746fbf8726d1c749
ale94 <alejandrorua111@gmail.com>
2025-08-20 21:31:24 -0300

    feat: add card employee component

---
+++ src/app/components/card-employee/card-employee.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-employee',
+  imports: [],
+  templateUrl: './card-employee.component.html',
+})
+export class CardEmployeeComponent { }
