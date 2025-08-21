70db8c6f4332712fa997b31c416c70cbb8aa8d09
ale94 <alejandrorua111@gmail.com>
2025-08-20 21:44:09 -0300

    feat: add input employee component

---
+++ src/app/components/input-employee/input-employee.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'input-employee',
+  imports: [],
+  templateUrl: './input-employee.component.html',
+})
+export class InputEmployeeComponent { }
